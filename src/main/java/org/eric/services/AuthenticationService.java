999a3f03f8c2c7af08972345446346886a7e1bb1
Eric Kinzel <ericrkinzel@gmail.com>
2018-01-13 15:41:07 -0600

    Added password hashing

--- src/main/java/org/eric/services/AuthenticationService.java
+++ src/main/java/org/eric/services/AuthenticationService.java
@@ -6,24 +6,15 @@ import java.security.NoSuchAlgorithmException;
 
 public class AuthenticationService {
 
-    public String md5(String plaintext){
-        String md5 = null;
-        
-		try {
-			
-            //Create MessageDigest object for MD5
-            MessageDigest digest = MessageDigest.getInstance("MD5");
+    public String encryptPassword(String password, String salt) {
+        try {
+            MessageDigest instance = MessageDigest.getInstance("MD5");
+            byte[] hashBytes = instance.digest((password + salt).getBytes());
+            String hash = new BigInteger(1, hashBytes).toString(16);
+            return hash;
             
-            //Update input string in message digest
-            digest.update(plaintext.getBytes(), 0, plaintext.length());
-
-            //Converts message digest value in base 16 (hex) 
-            md5 = new BigInteger(1, digest.digest()).toString(16);
-
-		} catch (NoSuchAlgorithmException e) {
-			e.printStackTrace();
+        } catch (NoSuchAlgorithmException ex) {
+            return "";
         }
-        
-		return md5;
     }
 }
