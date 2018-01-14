4616b3c74340ffb8d30c1994175378aacbb8d814
Eric Kinzel <ericrkinzel@gmail.com>
2018-01-14 11:04:58 -0600

    Adding to authentication service

--- src/main/java/org/eric/models/User.java
+++ src/main/java/org/eric/models/User.java
@@ -2,36 +2,40 @@ package org.eric.models;
 
 public class User {
 
-	private int id;
+    private int id;
     private String username;
-
-	/**
-	 * @return the username
-	 */
-	public String getUsername() {
-		return username;
-	}
-
-	/**
-	 * @param username the username to set
-	 */
-	public void setUsername(String username) {
-		this.username = username;
-	}
-
-	/**
-	 * @return the id
-	 */
-	public int getId() {
-		return id;
-	}
-
-	/**
-	 * @param id the id to set
-	 */
-	public void setId(int id) {
-		this.id = id;
-	}
+    private String salt;
+    private String password;
     
-    
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getSalt() {
+        return salt;
+    }
+
+    public void setSalt(String salt) {
+        this.salt = salt;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
--- src/main/java/org/eric/services/AuthenticationService.java
+++ src/main/java/org/eric/services/AuthenticationService.java
@@ -3,8 +3,21 @@ package org.eric.services;
 import java.math.BigInteger;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.sql.Connection;
+import java.sql.SQLException;
+import org.apache.commons.dbcp.BasicDataSource;
+import org.apache.commons.dbutils.DbUtils;
+import org.apache.commons.dbutils.QueryRunner;
+import org.apache.commons.dbutils.ResultSetHandler;
+import org.apache.commons.dbutils.handlers.BeanHandler;
+import org.eric.models.User;
 
-public class AuthenticationService {
+public class AuthenticationService extends BaseService{
+    
+    public AuthenticationService(BasicDataSource datasource){
+        super(datasource);
+    }
 
     public String encryptPassword(String password, String salt) {
         try {
@@ -15,6 +28,47 @@ public class AuthenticationService {
             
         } catch (NoSuchAlgorithmException ex) {
             return "";
+        }
+    }
+    
+    public String generateSalt(){
+        byte[] saltBytes = new byte[12];
+        SecureRandom rnd = new SecureRandom();
+        rnd.nextBytes(saltBytes);
+        
+        String asString = new BigInteger(1, saltBytes).toString(16);
+        return asString;
+    }
+    
+    public boolean validateCredentials(String username, String password){
+        User currentUser = getUserDetails(username);
+        String salt = currentUser.getSalt();
+        if(encryptPassword(password, salt).equals(currentUser.getPassword())){
+            return true;
+        }else{
+            return false;
+        }
+    }
+    
+    protected User getUserDetails(String username){
+        Connection conn = null;
+        
+        try{
+            conn = datasource.getConnection();
+            QueryRunner runner = new QueryRunner();
+            ResultSetHandler<User> handler = new BeanHandler<>(User.class);
+            
+            String sql = "SELECT * "
+                       + "FROM users "
+                       + "WHERE username = ? ";
+            
+            User theUser = runner.query(sql, handler, username);
+            return theUser;
+            
+        }catch(SQLException exc){
+            return new User();
+        }finally{
+            DbUtils.closeQuietly(conn);
         }
     }
 }
